fcfe5e4a8bf725a8ebd409721565336602b49a49
Raj2710 <nagarajansai2727@gmail.com>
2023-12-15 09:57:36 +0530

    Async Await

--- index.js
+++ index.js
@@ -106,22 +106,119 @@
 
 
 
-fetch('https://restcountries.com/v3.1/all',{method:'GET'})
-.then(req=>req.json())//string buffer to json
-.then(data=>createCards(data))//using the data
-.catch(error=>console.error(error))
+// fetch('https://restcountries.com/v3.1/all',{method:'GET'})
+// .then(req=>req.json())//string buffer to json
+// .then(data=>createCards(data))//using the data
+// .catch(error=>console.error(error))
+
+// function createCards(data)
+// {
+//     data.forEach((e)=>{
+//         console.log(e)
+
+//         let div = document.getElementById('root')
+//         let card = document.createElement('div')
+//         card.innerHTML=`<div class='card-wrapper'>
+//                             <h1>${e.name.common}</h1>
+//                             <img src="${e.flags.png}" />
+//                         </div>`
+//         div.appendChild(card)   
+//     })
+// }
+
+
+//Promise Day 2
+//Promise Chaining
+
+// let promise1 = new Promise((resolve,reject)=>{
+//         resolve("Promise 1 is Success")
+// })
+
+// let promise2 = new Promise((resolve,reject)=>{
+//         resolve("Promise 2 is success")
+// })
+
+// let promise3 = new Promise((resolve,reject)=>{
+//         resolve("Promise 3 is success")
+// })
+
+// let promise4 = new Promise((resolve,reject)=>{
+//         reject("Promise 4 is Rejected")
+// })
+
+// let promise5 = new Promise((resolve,reject)=>{
+//         resolve("Promise 5 is success")
+// })
+
+// promise1.then((values)=>{
+//     console.log(values)// Promise 1 outcome
+//     promise2.then(values=>{
+//         console.log(values)//promise 2 outcome
+//         promise3.then(values=>{
+//             console.log(values)//promise 3 outcome
+//             promise4.then(values=>{
+//                 console.log(values)//promise 4 outcome
+//                 promise5.then(values=>{
+//                     console.log(values)//promise 5 outcome
+//                 })
+//                 .catch(reason=>console.error(reason)) //Promise 5 Catch
+//             })
+//             .catch(reason=>console.error(reason)) //Promise 4 Catch
+//         })
+//         .catch(reason=>console.error(reason))//Promise 3 Catch
+//     })
+//     .catch(reason=>console.error(reason))//Promise 2 catch
+// })
+// .catch(reason=>console.error(reason))//Promise 1 catch
+
+
+//Disadvantages of Promise
+//1. It add or increase complexity to our code
+//2. Does not support in old browsers
+//3. Error handling is difficult as we need to handle for every indivudual promise we create.
+
+
+
+//Async Await
+
+async function getWeather(e) {
+    let {lat,lng} = e.currentTarget
+    console.log(lat,lng)
+
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=1df01ea2c0ed525b7685b8d43188acfd`)
+    let data = await res.json()
+    
+    let celcius = data.main.temp - 273.15
+    console.log(data.main.temp, celcius.toFixed(2))
+}
 
 function createCards(data)
 {
     data.forEach((e)=>{
-        console.log(e)
+        // console.log(e)
 
         let div = document.getElementById('root')
         let card = document.createElement('div')
+        let [lat,lng] = e.latlng
+        card.lat = lat
+        card.lng = lng
+        card.addEventListener('click',getWeather,false)
         card.innerHTML=`<div class='card-wrapper'>
                             <h1>${e.name.common}</h1>
                             <img src="${e.flags.png}" />
                         </div>`
-        div.appendChild(card)   
+        div.appendChild(card)
     })
 }
+
+async function loadData(){
+   try {
+        let res = await fetch('https://restcountries.com/v3.1/all',{method:'GET'})
+        let data = await res.json()
+        createCards(data)
+   } catch (error) {
+    console.error(error)
+   }
+}
+
+loadData()
